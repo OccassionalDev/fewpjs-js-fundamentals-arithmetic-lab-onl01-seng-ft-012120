928235f4b43ec93572a97652b7f7c6b1204a755a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 16:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const num2 = 2;
 
 const multiply = num1 * num2;
 const random = Math.floor(Math.random() * 10000) + 1;
-const mod = num1%4;
+const mod = 16%4;
 const max = Math.max(1, 2, 5, 10, 14, 20);
