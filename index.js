fc4a79692b81f4f91fa1a41e15a5ab8e63eee6bd
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 16:22:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Write your code here
 
-let multiply = num1 * num2;
-let random = Math.floor(Math.random() *10);
-let mod = num1%4;
-let max = Math.max(num1, num2, 20);
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() *10);
+const mod = num1%4;
+const max = Math.max(, num2, 20);
